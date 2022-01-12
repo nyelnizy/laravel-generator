bc93aefb222cb31092ffabaa8cf1bf3a72701357
nyelnizy <you@examplhiamdan@gmail.com>
2022-01-12 14:40:57 +0000

    updates

--- src/Commands/API/GenerateCrud.php
+++ src/Commands/API/GenerateCrud.php
@@ -85,7 +85,7 @@ class GenerateCrud extends Command
        if($is_graphql){
         Artisan::call("infyom:api $model_name
         --fieldsFile=$file_path --skip=views,menu,routes,controllers,tests
-        --seeder --factory $pfx --no-interaction");
+         --factory $pfx --no-interaction");
        }else{
         Artisan::call("infyom:api $model_name
         --fieldsFile=$file_path --skip=views,menu
