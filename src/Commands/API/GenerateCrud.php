b31f040d3d0e25f174108a198c21ace9e550bb0f
nyelnizy <you@examplhiamdan@gmail.com>
2022-01-12 14:31:54 +0000

    updates

--- src/Commands/API/GenerateCrud.php
+++ src/Commands/API/GenerateCrud.php
@@ -12,7 +12,7 @@ class GenerateCrud extends Command
      *
      * @var string
      */
-    protected $signature = 'crud:generate {model_name?} {file_name?} {--prefix} {--all=}';
+    protected $signature = 'crud:generate {model_name?} {file_name?} {--prefix} {--graphql} {--all=}';
 
     /**
      * The console command description.
@@ -44,6 +44,7 @@ class GenerateCrud extends Command
         if ($this->option('prefix')) {
             $pfx = "--prefix=$model_name";
         }
+        $is_graphql = $this->option('prefix');
         try {
             $model_files_path = config('infyom.laravel_generator.path.model_files');
             if (is_null($this->option('all'))) {
@@ -51,12 +52,12 @@ class GenerateCrud extends Command
                     if(!is_null($model_name)){
                         $file_name = strtolower($model_name) . '.json';
                     }else{
-                        throw  new \Exception('command usage, php artisan crud:generate ModelName model_file.json (model_file is optional)');
+                        throw  new \Exception('command usage, php artisan crud:generate ModelName model_file.json (model_file is optional if it has the same name as model)');
                     }
                     
                 }
                 $this->info("Generating Crud for $model_name...");
-                $this->generateCrud($model_name, "$model_files_path/$file_name", $pfx);
+                $this->generateCrud($model_name, "$model_files_path/$file_name", $pfx,$is_graphql);
                 $this->info("Generated Crud Successfully");
             } else {
                 $models = str_getcsv($this->option('all'), ',');
@@ -65,7 +66,7 @@ class GenerateCrud extends Command
                     $file_path = "$model_files_path/".strtolower($model_name) . '.json';
                     if (file_exists($file_path)) {
                         $this->info("$index) Generating Crud for $model_name...");
-                        $this->generateCrud($model_name,$file_path,$pfx);
+                        $this->generateCrud($model_name,$file_path,$pfx,$is_graphql);
                         $this->info("Generated Successfully");
                     } else {
                         $this->info("$index) Skipping Crud for $model_name, $file_path does not exist");
@@ -79,10 +80,16 @@ class GenerateCrud extends Command
         return 0;
     }
 
-    private function generateCrud(string $model_name, string $file_path, $pfx)
+    private function generateCrud(string $model_name, string $file_path, $pfx,$is_graphql)
     {
+       if($is_graphql){
+        Artisan::call("infyom:api $model_name
+        --fieldsFile=$file_path --skip=views,menu,routes,controllers,tests
+        --seeder --factory $pfx --no-interaction");
+       }else{
         Artisan::call("infyom:api $model_name
         --fieldsFile=$file_path --skip=views,menu
         --seeder --factory $pfx --no-interaction");
+       }
     }
 }
